aa38244ca199ce1d3717f8823ad5804ab425a268
Gazi Mahmudur Rahman <g.mahmudur.r@gmail.com>
2025-01-11 17:28:13 +0600

    add welcome function to greet users by name

---
+++ welcome.py
@@ -0,0 +1,10 @@
+# create a fuction called welcome, that takes a name as a parameter and returns a welcome message
+# The function should return "Welcome, " + name
+# Call the function and print the result
+# Expected output: Welcome, Mahmud
+
+def welcome(name):
+    return "Welcome, " + name
+
+print(welcome("Mahmud"))
+print(welcome("John"))
